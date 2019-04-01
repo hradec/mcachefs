f2d8d0be0f0d7ec0c80a5d3d4cda2eeb00a29d83
hradec <me@hradec.com>
2019-03-31 23:39:02 -0700

    test script will return error code -1 now if mcachefs crashes.

--- testing/testing-fclose-after-flush.py
+++ testing/testing-fclose-after-flush.py
@@ -14,7 +14,8 @@ os.system('mkdir -p '+BASEPATH+"/2")
 os.system("fusermount -u %s/2" % BASEPATH)
 
 # run mcachefs in gdb so we can see a stack trace in case it fails!
-cmd='''/bin/sh -c "echo -e 'r\\nbt\\n' | gdb --args %s -f -o -s %s/1 %s/2" ''' % (MCACHEFS, BASEPATH, BASEPATH)
+#cmd='''/bin/sh -c "echo -e 'r\\nbt\\n' | gdb --args %s -f -o -s %s/1 %s/2" ''' % (MCACHEFS, BASEPATH, BASEPATH)
+cmd='''/bin/sh -c "%s -f -o -s %s/1 %s/2" ''' % (MCACHEFS, BASEPATH, BASEPATH)
 p = subprocess.Popen(shlex.split(cmd))
 
 print shlex.split(cmd), p.pid
@@ -38,6 +39,12 @@ os.system('echo flush_metadata >  %s/2/.mcachefs/action' % BASEPATH)
 
 # close the opened file. It should NOT crash!
 f.close()
+
+time.sleep(3)
+l = os.popen( 'pgrep -fa "%s"' % MCACHEFS ).readlines()
+print l
+if not l:
+	print "mcachefs crashed!"
 
 # kill gdb if it's still running!
 p.poll()
